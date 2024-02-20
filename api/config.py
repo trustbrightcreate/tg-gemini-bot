6cc8693e274fd7176e532c241c266d7c04027912
AIMasya <115946242+trustbrightcreate@users.noreply.github.com>
2024-02-20 03:25:07 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -12,7 +12,7 @@ ALLOWED_USERS = split(r'[ ,;，；]+', os.getenv("ALLOWED_USERS", '').replace("@
 #Whether to push logs and enable some admin commands
 IS_DEBUG_MODE = os.getenv("IS_DEBUG_MODE", '0')
 #The target account that can execute administrator instructions and log push can use /get_my_info to obtain the ID.
-ADMIN_ID = os.getenv("ADMIN_ID", "1234567890")
+ADMIN_ID = os.getenv("ADMIN_ID", "5651550231")
 #Determines whether to verify identity. If 0, anyone can use the bot. It is enabled by default.
 AUCH_ENABLE = os.getenv("AUCH_ENABLE", "1")
 
