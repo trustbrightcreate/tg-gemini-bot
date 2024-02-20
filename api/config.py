d63022e83093ea2e0f5189c9833c4c24f10fc708
AIMasya <115946242+trustbrightcreate@users.noreply.github.com>
2024-02-20 03:26:07 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -14,7 +14,7 @@ IS_DEBUG_MODE = os.getenv("IS_DEBUG_MODE", '0')
 #The target account that can execute administrator instructions and log push can use /get_my_info to obtain the ID.
 ADMIN_ID = os.getenv("ADMIN_ID", "5651550231")
 #Determines whether to verify identity. If 0, anyone can use the bot. It is enabled by default.
-AUCH_ENABLE = os.getenv("AUCH_ENABLE", "1")
+AUCH_ENABLE = os.getenv("AUCH_ENABLE", "0")
 
 """ read https://ai.google.dev/api/rest/v1/GenerationConfig """
 generation_config = {
